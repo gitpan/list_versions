a6d8665f32824ca2bc8d4535b33d595e9de2f8e8
Zoffix Znet <zoffixcpan@gmail.com>
2008-02-07 03:31:56 +0000

    Import of ZOFFIX/list_versions-0.02 from CPAN.
    gitpan-cpan-distribution: list_versions
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ZOFFIX/list_versions-0.02.tar.gz
    gitpan-cpan-author:       ZOFFIX
    gitpan-cpan-maturity:     released

--- list_versions.pl
+++ list_versions.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 die "Usage: ver MODULE1 MODULE2 .. MODULEN\n"
     unless @ARGV;
@@ -19,18 +19,29 @@ foreach my $mod ( @ARGV ) {
 $max_length+=2;
 
 foreach my $mod ( @ARGV ) {
-    eval "use $mod; printf qq|%-${max_length}s => %s,$/|,"
+    eval "use $mod; printf qq|        %-${max_length}s => %s,$/|,"
             . "q|'$mod'|, $mod->VERSION;";
     if ( $@ ) {
         print "\nERROR: $@\n";
     }
 }
 
+print "\n\n";
+
+print qq|    use_ok('$_');\n| for @ARGV;
+
 =head1 NAME
 
-list_versions.pl - A little script that takes module names as command
+list_versions.pl - "easify" writing Makefile.PL and Build.PL by
+easily listing versions of modules.
+
+=head1 DESCRIPTION
+
+This is a tiny script that takes module names as command
 line arguments and prints the versions installed in a format suitable
-for copy/pasting into Build.PL or Makefile.PL
+for copy/pasting into Build.PL or Makefile.PL as well as
+basic C<use_ok()> test for each of them to include in L<Test::More>
+tests.
 
 =head1 USAGE
 
@@ -40,18 +51,31 @@ for copy/pasting into Build.PL or Makefile.PL
 
 This is a sample of printed output for the USAGE sample:
 
-    'Carp'              => 1.04,
-    'URI'               => 1.35,
-    'LWP::UserAgent'    => 2.036,
+            'Carp'              => 1.04,
+            'URI'               => 1.35,
+            'LWP::UserAgent'    => 2.036,
 
-    ERROR: Can't locate Some/OtherModule.pm in @INC (@INC contains: /etc/perl /usr/local/lib/perl/5.8.8 /usr/local/share/perl/5.8.8 /usr/lib/perl5 /usr/share/perl5 /usr/lib/perl/5.8 /usr/share/perl/5.8 /usr/local/lib/site_perl .) at (eval 7) line 1.
+    ERROR: Can't locate Some/OtherModule.pm in @INC (@INC contains:
+    /etc/perl /usr/local/lib/perl/5.8.8 /usr/local/share/perl/5.8.8
+    /usr/lib/perl5 /usr/share/perl5 /usr/lib/perl/5.8 /usr/share/perl/5.8
+    /usr/local/lib/site_perl .) at (eval 7) line 1.
     BEGIN failed--compilation aborted at (eval 7) line 1.
 
-    'LWP'               => 5.808,
+            'LWP'               => 5.808,
+
+
+        use_ok('Carp');
+        use_ok('URI');
+        use_ok('LWP::UserAgent');
+        use_ok('Some::OtherModule');
+        use_ok('LWP');
+
 
 =head1 AUTHOR
 
 Zoffix Znet (zoffix@cpan.org)
+
+( L<http://zoffix.com>, L<http://haslayout.net> )
 
 =head1 LICENSE
 
