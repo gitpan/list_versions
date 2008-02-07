4b3c3af3dd841643e42272cca53f53f07334e8b4
Zoffix Znet <zoffixcpan@gmail.com>
2008-02-07 00:56:25 +0000

    Import of ZOFFIX/list_versions-0.01 from CPAN.
    gitpan-cpan-distribution: list_versions
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ZOFFIX/list_versions-0.01.tar.gz
    gitpan-cpan-author:       ZOFFIX
    gitpan-cpan-maturity:     released

---
+++ list_versions.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+die "Usage: ver MODULE1 MODULE2 .. MODULEN\n"
+    unless @ARGV;
+
+my $max_length = 0;
+
+foreach my $mod ( @ARGV ) {
+    my $l = length $mod;
+    $max_length = $l
+        if $l > $max_length;
+}
+
+$max_length+=2;
+
+foreach my $mod ( @ARGV ) {
+    eval "use $mod; printf qq|%-${max_length}s => %s,$/|,"
+            . "q|'$mod'|, $mod->VERSION;";
+    if ( $@ ) {
+        print "\nERROR: $@\n";
+    }
+}
+
+=head1 NAME
+
+list_versions.pl - A little script that takes module names as command
+line arguments and prints the versions installed in a format suitable
+for copy/pasting into Build.PL or Makefile.PL
+
+=head1 USAGE
+
+    list_versions.pl Carp URI LWP::UserAgent Some::OtherModule LWP
+
+=head1 OUTPUT
+
+This is a sample of printed output for the USAGE sample:
+
+    'Carp'              => 1.04,
+    'URI'               => 1.35,
+    'LWP::UserAgent'    => 2.036,
+
+    ERROR: Can't locate Some/OtherModule.pm in @INC (@INC contains: /etc/perl /usr/local/lib/perl/5.8.8 /usr/local/share/perl/5.8.8 /usr/lib/perl5 /usr/share/perl5 /usr/lib/perl/5.8 /usr/share/perl/5.8 /usr/local/lib/site_perl .) at (eval 7) line 1.
+    BEGIN failed--compilation aborted at (eval 7) line 1.
+
+    'LWP'               => 5.808,
+
+=head1 AUTHOR
+
+Zoffix Znet (zoffix@cpan.org)
+
+=head1 LICENSE
+
+There is no license. You may do whatever you want with this program and
+author is in no way reliable for ANYTHING that will happen.
+
+=cut
